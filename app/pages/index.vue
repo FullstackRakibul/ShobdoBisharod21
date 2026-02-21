bb396516926f57f37b60490b88a8987d53ce0790
FullstackRakibul <rakibul.cse.47@gmail.com>
2026-02-22 00:05:27 +0600

    description added

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,7 @@ useHead({
     { name: 'publisher', content: '@fullstackrakibul' },
     { name: 'description', content: 'আপনার বাংলা শব্দভান্ডার যাচাই করুন — ২১টি শব্দ দিয়ে পরীক্ষা করুন কতটি খাঁটি বাংলা!' },
     { property: 'og:title', content: 'শব্দবিশারদ ২১ — বাংলা শব্দ খেলা' },
-    { property: 'og:description', content: '২১শে ফেব্রুয়ারি উপলক্ষে আপনার বাংলা শব্দভান্ডার পরীক্ষা করুন' },
+    // { property: 'og:description', content: '২১শে ফেব্রুয়ারি উপলক্ষে আপনার বাংলা শব্দভান্ডার পরীক্ষা করুন' },
   ],
 })
 </script>
